e93e0de4f038fc083f583f348f3d1db5d4ff3067
bhanu <bhanuprasadmende@gmail.com>
2014-09-24 18:21:56 +0530

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,28 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def aboutus
+  end
+
+  def aboutus1
+  end
+
+  def contactus
+  end
+
+  def specifications
+  end
+  
+  def gallery
+  end
+
+  def enquiry
+  end
+
+  def products
+  end
+  def try
+  end
+  
+end
